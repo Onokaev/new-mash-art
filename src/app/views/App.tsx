13e4a2407610095bfd2ccce65c832e01b0cafdaa
Onokaev <evansonoka05.j@gmail.com>
2022-04-05 00:10:26 +0300

    remove unused code

--- src/app/views/App.tsx
+++ src/app/views/App.tsx
@@ -1,13 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ArtSection } from './app-sections/art-section';
 import { Navigation } from './app-sections/nav-section/Navigation';
 import { AppTitle } from './app-sections/title-section/AppTitle';
 import { appStyles } from './App.styles';
 const App = () => {
   const app = appStyles().app;
-  const [soldPieces, setSoldPieces ] = useState([]);
-  const [portfolioPieces, setPortFolioPieces ] = useState([]);
-  const [availablePieces, setAvailablePieces ] = useState([]);
   
   return (
     <div style={app}>
