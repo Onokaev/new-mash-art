3cf58806e5a5591d307ca8aa81b7553802a57d96
Onokaev <evansonoka05.j@gmail.com>
2022-04-05 00:12:37 +0300

    lint

--- src/app/views/app-sections/art-section/ArtSection.tsx
+++ src/app/views/app-sections/art-section/ArtSection.tsx
@@ -1,10 +1,4 @@
 import React from 'react';
-
-interface IArtSectionProps {
-    artName: String;
-    artSource: String;
-    artID: Number;
-}
 export const ArtSection = () => {
     return(
         <div>
